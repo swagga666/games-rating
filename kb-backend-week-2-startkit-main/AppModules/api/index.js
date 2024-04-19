36871d821c968edab7279b10c23e4b26fc4802d7
swagga666 <167466462+swagga666@users.noreply.github.com>
2024-04-19 12:43:41 +0300

    Delete kb-backend-week-2-startkit-main directory

--- kb-backend-week-2-startkit-main/AppModules/api/index.js
+++
@@ -1,8 +0,0 @@
-const { getData, getRandomGame } = require("./api-utils");
-const endpoints = require("./config")
-
-module.exports = {
-    endpoints,
-    getData,
-    getRandomGame,
-};
--- kb-backend-week-2-startkit-main/AppModules/http-utils/index.js
+++
@@ -1,9 +0,0 @@
-const mimeTypes = require("./mime-types");
-const parseBody = require("./parse-body");
-const staticFile = require("./static-file");
-
-module.exports = {
-    mimeTypes,
-    staticFile,
-    parseBody,
-};
